0530fa305d10ece613c253ad25fd09544985d59b
mozamel-hmd <99012621+mozamel-hmd@users.noreply.github.com>
2023-02-17 16:18:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-let table = ["carle","bastien","seb","sam"];
-fetch(table)
-.then(reponse => reponse.json())
-.then(data =>{
-console.log(data)
-})
 
+var niveau1 = {
+    "title": "Découverte",
+    "creator": "Mozamel Himidi",
+    "difficulty": 1,
+    "blocks": [
+        {"type": "espace vide"},
+        {"type": "bloc sol"},
+        {"type": "espace vide"},
+        {"type": "bloc en l'air"},
+        {"type": "espace vide"},
+        {"type": "bloc sol"},
+        {"type": "bloc en l'air"},
+    ]
+};
+
+let bouton = document.getElementById("bouton1");
+bouton.addEventListener("click", function () {
+    console.log(niveau1.blocks[0]);
+    alert("vous avez placer un espace vide");
+});
+
+let boutonx = document.getElementById("bouton2");
+boutonx.addEventListener("click", function () {
+    console.log(niveau1.blocks[1]);
+    alert("vous avez placer un bloc sol");
+});
+
+let boutony = document.getElementById("bouton3");
+boutony.addEventListener("click", function () {
+        console.log(niveau1.blocks[3]);
+        alert("vous avez placer un bloc en l'air");
+});
+
+// Charger le fichier JSON
+fetch('fichier.json')
+  .then(response => response.json())
+  .then(data => {
+    // Récupérer l'élément HTML
+    var monElement = document.getElementsByClassName("constructeur");
+
+    // Ajouter le texte à l'élément HTML
+    monElement.innerText = data.blocks;
+  });
